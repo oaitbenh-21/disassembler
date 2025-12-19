b7924be4f53e25d406d160842a5813b62984a14b
omar ait benhammou <0xmrrandom@gmail.com>
2025-12-19 14:33:10 +0100

    disassembler work successfully but there an error in assembler and i should to replace unwrap's with match or functions that check if there a value before unwrap

--- disassembler/player.s
+++ disassembler/player.s
@@ -16,3 +16,19 @@ fork %20
 lld 10, r1
 lldi r1, r2, r3
 lfork %30
+
+#****** live %1
+#****** ld 34 r2;
+#****** st r2 %45;
+#****** add r2 r3 r4;
+#****** sub r4 r2 r1;
+#****** and r1 10 r2;
+#****** or r2 r3 r4;
+#****** xor r4 r1 r2;
+#****** zjmp %10
+#****** ldi r2 5 r3;
+#****** sti r3 r2 2;
+#****** fork %20
+#****** lld 10 r1;
+#****** lldi r1 2 r3;
+#****** lfork %30
--- disassembler/src/module/instruction.rs
+++ disassembler/src/module/instruction.rs
@@ -6,25 +6,16 @@ use shared::{
 pub struct InstructionValues<'a> {
     pub opcode: &'a u8,
     pub pcode: &'a u8,
-    pub param: Vec<ParamTypeAndValue>,
-}
-
-pub enum ParamTypeAndValue {
-    Register(u8),
-    Direct(u16),
-    DirectHasIdx(u32),
-    Indirect(u64),
 }
 
 impl<'a> InstructionValues<'a> {
     pub fn get_instruction_data(instructions_slice: Vec<u8>, code_size: &u32) {
-        println!("instructions: {:?}", instructions_slice);
+        // println!("instructions: {:?}", instructions_slice);
         let mut decoded_byte_count: usize = 0;
         while decoded_byte_count < *code_size as usize {
             let mut current_instruction_values = InstructionValues {
                 opcode: &(0),
                 pcode: &(0),
-                param: (Vec::new()),
             };
             current_instruction_values.opcode =
                 &instructions_slice.get(decoded_byte_count).unwrap();
@@ -37,14 +28,14 @@ impl<'a> InstructionValues<'a> {
                     &instructions_slice.get(decoded_byte_count).unwrap();
                 decoded_byte_count += 1;
                 for (i, _) in current_instruction_data.params.iter().enumerate() {
-                    let pcode = (*current_instruction_values.pcode) >> (6 - i * 2) | 0b00;
+                    let pcode = (*current_instruction_values.pcode >> (6 - i * 2)) & 0b11; //(pc >> (6 - i * 2)) & 0b11
                     match pcode {
                         0b11 => {
                             // ParamType::Indirect == i16
                             let value = instructions_slice
                                 .get(decoded_byte_count as usize..decoded_byte_count as usize + 2)
                                 .unwrap();
-                            print!(" %{:?}", read_i16_be(value, &mut 0));
+                            print!(" %{}", read_i16_be(value, &mut 0).unwrap());
                             decoded_byte_count += 2;
                         }
                         0b10 => {
@@ -71,16 +62,18 @@ impl<'a> InstructionValues<'a> {
                         }
                         0b01 => {
                             // ParamType::Register u8
-                            let value = instructions_slice
-                                .get(decoded_byte_count as usize..decoded_byte_count)
-                                .unwrap();
-                            print!(" r{}", value[0]);
+                            let value =
+                                instructions_slice.get(decoded_byte_count as usize).unwrap();
+                            print!(" r{}", value);
                             decoded_byte_count += 1;
                         }
-                        _ => {}
+                        _ => {
+                            println!("\npcode doesn't match anything");
+                        }
                     }
-                    println!(";")
                 }
+                println!(";");
+                // println!("pcode: {:b}", current_instruction_values.pcode);
             } else {
                 for params_arr in current_instruction_data.params {
                     if matches!(params_arr[0], ParamType::Direct) {
